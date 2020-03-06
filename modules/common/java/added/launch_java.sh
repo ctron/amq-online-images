54ac543ddaf5559625080bb9681d3f1ad89e0d8e
Jens Reimann <jreimann@redhat.com>
2020-03-06 11:42:11 +0100

    Drop the requirement to use "-jar" and let the containers decide

--- modules/common/java/added/launch_java.sh
+++ modules/common/java/added/launch_java.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 set -x
-JAR=$1
-shift
 
 source /usr/local/dynamic-resources/dynamic_resources.sh
 
@@ -12,4 +10,4 @@ LOG_LEVEL=${LOG_LEVEL:-info}
 # Make sure that we use /dev/urandom
 JAVA_OPTS="${JAVA_OPTS} -DLOG_LEVEL=${LOG_LEVEL} -Dvertx.disableDnsResolver=true -Dvertx.cacheDirBase=/tmp -Djava.security.egd=file:/dev/./urandom"
 
-exec java $JAVA_OPTS -jar $JAR $JAVA_OPTS $@
+exec java $JAVA_OPTS $JAVA_OPTS $@
